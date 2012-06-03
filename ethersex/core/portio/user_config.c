0a56c9f1bfeaf5d731d476bd79c499e6f6a5076b
Achim <achim.krauch@googlemail.com>
2012-06-03 23:44:52 +0200

    Ethersex-Website mit *STB?-Abfrage

--- ethersex/core/portio/user_config.c
+++ ethersex/core/portio/user_config.c
@@ -3,17 +3,17 @@
 #define INPUT 1
 #define LOW 0
 #define HIGH 1
-const char named_pin_text2917[] PROGMEM = "p1";
-const char named_pin_text12431[] PROGMEM = "p2";
-const char named_pin_text3462[] PROGMEM = "p3";
-const char named_pin_text1272[] PROGMEM = "p4";
+const char named_pin_text19544[] PROGMEM = "p1";
+const char named_pin_text21441[] PROGMEM = "p2";
+const char named_pin_text18129[] PROGMEM = "p3";
+const char named_pin_text23547[] PROGMEM = "p4";
 
 const struct PinConfiguration portio_pincfg[] PROGMEM = {
     /*  port	pin	input	reverse? */
-	{	0,	0,	OUTPUT,	HIGH,	named_pin_text2917 }, 	/* p1	*/ 
-	{	0,	1,	OUTPUT,	HIGH,	named_pin_text12431 }, 	/* p2	*/ 
-	{	0,	2,	OUTPUT,	HIGH,	named_pin_text3462 }, 	/* p3	*/ 
-	{	0,	3,	OUTPUT,	HIGH,	named_pin_text1272 }, 	/* p4	*/ 
+	{	0,	0,	OUTPUT,	HIGH,	named_pin_text19544 }, 	/* p1	*/ 
+	{	0,	1,	OUTPUT,	HIGH,	named_pin_text21441 }, 	/* p2	*/ 
+	{	0,	2,	OUTPUT,	HIGH,	named_pin_text18129 }, 	/* p3	*/ 
+	{	0,	3,	OUTPUT,	HIGH,	named_pin_text23547 }, 	/* p4	*/ 
 
     /* mark the end of the list */
     { 255, 255, 255, 255, NULL}
